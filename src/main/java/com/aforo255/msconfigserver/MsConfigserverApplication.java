515a230a7acb4b4986520c159842232c805fb67d
Mauricio Manjarres <mauricio.manjarres@hotmail.com>
2021-06-08 13:22:56 -0500

    Conectando Github

---
+++ src/main/java/com/aforo255/msconfigserver/MsConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.aforo255.msconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigserverApplication.class, args);
+    }
+
+}
